612d739c5b2d33418beb14735c4e8b43b7c71a90
devarturobl <4rthurbl00@gmail.com>
2024-09-25 14:08:36 -0600

    2509

--- Semana 5/variables.js
+++ Semana 5/variables.js
@@ -31,7 +31,99 @@ console.log(250/num1);
 console.log(num1/num2);
 //Modulo
 console.log(num1%num2);
+
 //Prueba de precedencia
 console.log(3 + 5 * 2 - 8 * 5 / 2);
 console.log(3 / 2 + 5 * 4 - 3 + 100 / 2 * 4, "El resultado");
+console.log("");
+console.log("Segundo bloque de ejercicios operadores relacionales");
+
+//Operadores de comparación o relacionales
+//Ejemplos <,>,<=,>=,== y !=
+console.log("4 es mayor que 7: ", 4 > 7);
+console.log("4 es menor que 7: ", 4 < 7);
+console.log("4 es mayor o igual que 7: ", 4 >= 7);
+console.log("4 es nemor o igual que 7: ", 4 <= 7);
+console.log("4 es igual que 7: ", 4 == 7);
+console.log("4 es diferente a 7: ", 4 != 7);
+
+//Igualdad vs igualdad extricta
+console.log("Igualdad extricta");
+console.log(3 == 3);
+console.log(3 === 3);
+console.log(3 == "3");
+console.log(3 === "3");
+console.log("BOB ESPONJA");
+console.log(0 == "0");
+console.log(0 == []);
+console.log("0" == []);
+console.log(true == 1);
+console.log("Estrella",false === 0);
+
+//Asociatividad
+console.log("");
+console.log("Asociatividad");
+var a = 20;
+var b = 22;
+var c = (a = b) + 60;
+
+console.log("Valor de a: ",a);
+console.log("Valor de b: ",b);
+console.log("Valor de c: ",c);
+
+console.log(16 / 2 / 4);
+
+//Nota las operaciones +,/,-,*,etc son de izq a derecha
+//las comparaciones <,>,<=,>=,== y != son de derecha a izq
+
+//Funciones en JS
+//Ejemplo 1 decalarando una funcion sin parametros
+function saludar(){
+    console.log("Hola como estas, usaste la función saludar");
+}
+
+//Ejemplo 1 usando la funcion
+saludar();saludar();saludar();saludar();saludar();saludar();saludar();
+
+//Ejemplo 2 Encapsular los ejercicios anteriores
+function operacionesComparacion(){
+    console.log("4 es mayor que 7: ", 4 > 7);
+    console.log("4 es menor que 7: ", 4 < 7);
+    console.log("4 es mayor o igual que 7: ", 4 >= 7);
+    console.log("4 es nemor o igual que 7: ", 4 <= 7);
+    console.log("4 es igual que 7: ", 4 == 7);
+    console.log("4 es diferente a 7: ", 4 != 7);
+}
+
+operacionesComparacion();
+
+function igualdadExtricta(){
+    console.log("Igualdad extricta");
+    console.log(3 == 3);
+    console.log(3 === 3);
+    console.log(3 == "3");
+    console.log(3 === "3");
+    console.log("BOB ESPONJA");
+    console.log(0 == "0");
+    console.log(0 == []);
+    console.log("0" == []);
+    console.log(true == 1);
+    console.log("Estrella",false === 0);
+}
+
+igualdadExtricta();
+
+function suma(a, b) {
+    return a + b;
+}
+
+var sumaDos = (x, y) => {
+    return x + y;
+  }
+
+var rsuma = suma(10,9);
+console.log(rsuma);
+
+var rsuma2 = sumaDos(50,60);
+console.log(rsuma2);
 
