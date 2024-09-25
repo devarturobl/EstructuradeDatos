9a8ea2ae07afa8ca6f6bee4398c7dbe4f732e549
devarturobl <4rthurbl00@gmail.com>
2024-09-25 12:16:45 -0600

    2309

---
+++ Semana 5/variables.js
@@ -0,0 +1,37 @@
+//Ejercicio de Variables en js Declaración de variables con var
+//Variables de tipo String no hay diferencia si uso comilla simple o doble
+// este es el error var nombre : "Felipe";
+//esta es la solicion
+var nombre = "Felipe";
+var ciudad = 'Ajalpan';
+//Variables de tipo número
+var entero = 90;
+var negativo = -120;
+var decimal = 19.67;
+//Variables de tipo Booleano
+var verdad = true;
+var mentira = false;
+//Variable de tipo Undifined
+var noseeltipo;
+//Variable Null
+var nula = null;
+
+//Operadores matematicos en js
+//variables contenedoras
+var num1, num2;
+//Suma
+num1 = 50;
+console.log(50 + 5);
+console.log(num1 + 5);
+//Resta
+num2 = 20;
+console.log(num1-num2);
+//División
+console.log(250/num1);
+console.log(num1/num2);
+//Modulo
+console.log(num1%num2);
+//Prueba de precedencia
+console.log(3 + 5 * 2 - 8 * 5 / 2);
+console.log(3 / 2 + 5 * 4 - 3 + 100 / 2 * 4, "El resultado");
+
